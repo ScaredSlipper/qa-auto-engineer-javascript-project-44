3c130f7821dd054580a20b3568f1549f43a7e448
ScaredSlipper <8361517@gmail.com>
2026-05-11 22:57:17 +0300

    implemented name asking feature

---
+++ src/cli.js
@@ -0,0 +1,8 @@
+import readLineSync from 'readline-sync'
+
+const askName = () => {
+    let userName = readLineSync.question('May I have your name? ')
+    console.log(`Hello, ${userName}!`)
+}
+
+export default askName
