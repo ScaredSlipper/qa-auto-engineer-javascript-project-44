514426fb891d18140e52703d1c5dbc6b4a7f2809
ScaredSlipper <8361517@gmail.com>
2026-05-13 21:23:17 +0300

    fix lint errors

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
 import readLineSync from 'readline-sync'
 
 const askName = () => {
-    let userName = readLineSync.question('May I have your name? ')
-    console.log(`Hello, ${userName}!`)
+  let userName = readLineSync.question('May I have your name? ')
+  console.log(`Hello, ${userName}!`)
 }
 
 export default askName
