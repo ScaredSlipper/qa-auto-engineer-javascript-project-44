f605b71a1322b94c87d14ebcff97f0ff04e4b3e0
ScaredSlipper <8361517@gmail.com>
2026-05-16 11:19:08 +0300

    restructure brain-games-lgc.js

--- src/brain-games-lgc.js
+++ src/brain-games-lgc.js
@@ -3,19 +3,31 @@ import _ from 'lodash'
 const getGameSpecifics = (game) => {
   switch (game) {
     case 'brainEven': {
+      const [gameMessage, gameQuestion, expectedAnswer] = getGameBrainEvenSpec()
+      return [gameMessage, gameQuestion, expectedAnswer]
+    }
+    case 'brainCalc': {
+      const [gameMessage, gameQuestion, expectedAnswer] = getGameBrainCalcSpec()
+      return [gameMessage, gameQuestion, expectedAnswer]
+    }
+
+export default getGameSpecifics
+
+const getGameBrainEvenSpec = () => {
       const gameMessage = 'Answer "yes" if the number is even, otherwise answer "no".'
 
-      const gameQuestion = Math.floor(Math.random() * 100)
+  const gameQuestion = _.random(99)
 
       const expectedAnswer = gameQuestion % 2 === 0 ? 'yes' : 'no'
 
       return [gameMessage, gameQuestion, expectedAnswer]
     }
-    case 'brainCalc': {
+
+const getGameBrainCalcSpec = () => {
       const gameMessage = 'What is the result of the expression?'
 
-      const firstNum = Math.floor(Math.random() * 100)
-      const secondNum = Math.floor(Math.random() * 100)
+  const firstNum = _.random(99)
+  const secondNum = _.random(99)
       const operatorList = ['+', '-', '*']
       const operator = _.sample(operatorList)
       const gameQuestion = `${firstNum} ${operator} ${secondNum}`
