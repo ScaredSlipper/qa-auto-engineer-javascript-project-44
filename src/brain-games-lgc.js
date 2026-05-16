81d19c29af0208638493f94be01bd398ada22678
ScaredSlipper <8361517@gmail.com>
2026-05-16 11:24:32 +0300

    add game brain-gcd

--- src/brain-games-lgc.js
+++ src/brain-games-lgc.js
@@ -10,44 +10,67 @@ const getGameSpecifics = (game) => {
       const [gameMessage, gameQuestion, expectedAnswer] = getGameBrainCalcSpec()
       return [gameMessage, gameQuestion, expectedAnswer]
     }
-
-export default getGameSpecifics
-
-const getGameBrainEvenSpec = () => {
-      const gameMessage = 'Answer "yes" if the number is even, otherwise answer "no".'
-
-  const gameQuestion = _.random(99)
-
-      const expectedAnswer = gameQuestion % 2 === 0 ? 'yes' : 'no'
-
+    case 'brainGCD': {
+      const [gameMessage, gameQuestion, expectedAnswer] = getGameBrainGCDSpec()
       return [gameMessage, gameQuestion, expectedAnswer]
-    }
-
-const getGameBrainCalcSpec = () => {
-      const gameMessage = 'What is the result of the expression?'
-
-  const firstNum = _.random(99)
-  const secondNum = _.random(99)
-      const operatorList = ['+', '-', '*']
-      const operator = _.sample(operatorList)
-      const gameQuestion = `${firstNum} ${operator} ${secondNum}`
-
-      switch (operator) {
-        case '+': {
-          const expectedAnswer = _.toString(firstNum + secondNum)
-          return [gameMessage, gameQuestion, expectedAnswer]
-        }
-        case '-': {
-          const expectedAnswer = _.toString(firstNum - secondNum)
-          return [gameMessage, gameQuestion, expectedAnswer]
-        }
-        case '*': {
-          const expectedAnswer = _.toString(firstNum * secondNum)
-          return [gameMessage, gameQuestion, expectedAnswer]
-        }
-      }
     }
   }
 }
 
 export default getGameSpecifics
+
+const getGameBrainEvenSpec = () => {
+  const gameMessage = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+  const gameQuestion = _.random(99)
+
+  const expectedAnswer = gameQuestion % 2 === 0 ? 'yes' : 'no'
+
+  return [gameMessage, gameQuestion, expectedAnswer]
+}
+
+const getGameBrainCalcSpec = () => {
+  const gameMessage = 'What is the result of the expression?'
+
+  const firstNum = _.random(99)
+  const secondNum = _.random(99)
+  const operatorList = ['+', '-', '*']
+  const operator = _.sample(operatorList)
+  const gameQuestion = `${firstNum} ${operator} ${secondNum}`
+
+  switch (operator) {
+    case '+': {
+      const expectedAnswer = _.toString(firstNum + secondNum)
+      return [gameMessage, gameQuestion, expectedAnswer]
+    }
+    case '-': {
+      const expectedAnswer = _.toString(firstNum - secondNum)
+      return [gameMessage, gameQuestion, expectedAnswer]
+    }
+    case '*': {
+      const expectedAnswer = _.toString(firstNum * secondNum)
+      return [gameMessage, gameQuestion, expectedAnswer]
+    }
+  }
+}
+
+const getGameBrainGCDSpec = () => {
+  const gameMessage = 'Find the greatest common divisor of given numbers.'
+
+  const firstNum = _.random(99)
+  const secondNum = _.random(99)
+  const gameQuestion = `${firstNum} ${secondNum}`
+
+  const expectedAnswer = _.toString(calculateGCD(firstNum, secondNum))
+
+  return [gameMessage, gameQuestion, expectedAnswer]
+}
+
+const calculateGCD = (firstNum, secondNum) => {
+  while (secondNum !== 0) {
+    let temp = secondNum
+    secondNum = firstNum % secondNum
+    firstNum = temp
+  }
+  return firstNum
+}
